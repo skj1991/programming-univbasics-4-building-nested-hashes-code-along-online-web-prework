5a5b8ab301742a2d3f9d8381090e011f16a6cf6a
Stacy Johns <stacykjohns@icloud.com>
2019-07-12 21:41:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,8 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
      }
    }
 end
